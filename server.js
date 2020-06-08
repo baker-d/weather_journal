c760bfbf68ce013db9482485ab221757bfa7b355
David Baker <david.a.baker@me.com>
2020-06-07 23:41:36 -0500

    Add files via upload
    bug fixes, CSS styling added, HTML tags added, updateUI, refactored code

--- server.js
+++ server.js
@@ -1,34 +1,59 @@
 /* Empyt JS object to act as endpoint for all routes */
-// projectData = {};
+projectData = { data: [] };
 
 // Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instancec of app
 const app = express();
 
 // Dependencies
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 /* Middleware */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Todo- Cors for X origin allowance
-const cors = require('cors');
+// Todo - Cors for X origin allowance
+const cors = require("cors");
 app.use(cors());
 
 // Initialize main project folder
-app.use(express.static('weather_journal'));
+app.use(express.static("weather_journal"));
 
 const port = 8000;
 
 // Spin up server
 const server = app.listen(port, listening);
-//const server - app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
 
 // Callback to debug
-function listening(){
-    console.log('server running');
-    console.log(`running on localhost: ${port}`);
-    
+function listening() {
+  console.log("Server running");
+  console.log(`running on localhost: ${port}`);
 }
+
+// Routers
+app.get("/", sendData);
+
+function sendData(req, res) {
+  res.sendFileO(__dirname + "index.html");
+  res.send(projectData);
+}
+
+app.post("/db", (req, res) => {
+  const { temp, currentTime, userResponse } = req.body.main;
+  const returnedTemp = `${temp} \u00B0 F`;
+  const returnedCurrentDate = currentTime;
+  const returnedUserRespone = userResponse;
+
+  projectData.data.push({
+    temperature: returnedTemp,
+    currentDate: returnedCurrentDate,
+    userResponse: returnedUserRespone,
+  });
+
+  res.send(projectData);
+});
+
+app.get("/all", (req, res) => {
+  res.send(projectData);
+});
